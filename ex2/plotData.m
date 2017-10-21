084f1490d74185aa600b0a6ce9b12239ae05b556
Ivan Oreskovic <ivan.oreskovic@gmail.com>
2017-10-21 19:00:31 +0200

    Plotting data
    Positive - green filled circle
    Negative - red filled diamond

--- ex2/plotData.m
+++ ex2/plotData.m
@@ -11,18 +11,12 @@ figure; hold on;
 %               2D plot, using the option 'k+' for the positive
 %               examples and 'ko' for the negative examples.
 %
+pos = find(y == 1);
+neg = find(y == 0);
 
-
-
-
-
-
-
-
-
+plot(X(pos, 1), X(pos, 2), "ko", "LineWidth", 1, "MarkerEdgeColor", "k", "MarkerFaceColor", "g", "MarkerSize", 5)
+plot(X(neg, 1), X(neg, 2), "kd", "LineWidth", 1, "MarkerEdgeColor", "k", "MarkerFaceColor", "r", "MarkerSize", 5)
 % =========================================================================
-
-
 
 hold off;
 
