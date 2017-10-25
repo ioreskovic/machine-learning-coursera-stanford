593a0b1c8bd979710a1ceb1f55d85b71f8e0af82
Ivan Oreskovic <ivan.oreskovic@gmail.com>
2017-10-25 17:02:31 +0200

    Multi-classification

--- ex3/predictOneVsAll.m
+++ ex3/predictOneVsAll.m
@@ -29,13 +29,8 @@ X = [ones(m, 1) X];
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
 %       
-
-
-
-
-
-
-
+classMatrix = X * all_theta';
+[v, p] = max(classMatrix, [], 2);
 % =========================================================================
 
 
